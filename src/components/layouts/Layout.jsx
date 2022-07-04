3652e992905bad72dbc35d64b519e833a7322038
Edwcassa <edwar.cassa@gmail.com>
2022-07-03 20:49:42 -0500

    add navigation header

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Header from '../Header'
 
 export default function Layout({ children }) {
   return (
     <>
-      <h1>Header</h1>
+      <Header />
       {children}
-      <h1>Footer</h1>
+      {/* <h1>Footer</h1> */}
     </>
   )
 }
